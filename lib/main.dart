7a67620aecbf22dbe3d5ae3ad3733c06d4b0b1ba
junehan <junehan.dev@gmail.com>
2022-10-07 16:02:30 +0900

    FEAT/main/login button as container

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,61 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+	runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+	const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-			centerTitle: true,
-			title: Text(
-				"Hello Flutter",
-				style: TextStyle(
-					fontSize: 28
-				),
-			)
-		),
-		body: Column(
-			children: [
-				Text(
-					"Hello Flutter",
-					style: TextStyle(
-						fontSize: 28
+	@override
+	Widget build(BuildContext context) {
+		return MaterialApp(
+			debugShowCheckedModeBanner: false,
+			home: Scaffold(
+				appBar: AppBar(
+					centerTitle: true,
+					title: Text(
+						"Hello Flutter",
+						style: TextStyle(
+							fontSize: 28
+						),
 					)
 				),
-				TextField(
-					decoration: InputDecoration(
-						labelText: "email"
-					)
-				),
-				TextField(
-					obscureText: true,
-					decoration: InputDecoration(
-						labelText: "password"
-					)
-				),
-				ElevatedButton(
-					onPressed: () {},
-					child: Text(
-						"sign in"
-					)
-				),
-			]
-		),
-      ),
-    );
-  }
+				body: Padding(
+					padding: const EdgeInsets.all(8.0),
+					child: Column(
+						children: [
+							Text(
+								"Hello Flutter",
+								style: TextStyle(
+									fontSize: 28
+								)
+							),
+							TextField(
+								decoration: InputDecoration(
+									labelText: "email"
+								)
+							),
+							TextField(
+								obscureText: true,
+								decoration: InputDecoration(
+									labelText: "password"
+								)
+							),
+							Container(
+								width: double.infinity,
+								margin: EdgeInsets.only(top:24),
+								child: ElevatedButton(
+										onPressed: () {},
+										child: Text(
+											"sign in"
+										)
+									),
+							)
+						]
+					),
+				)
+			),
+		);
+	}
 }
